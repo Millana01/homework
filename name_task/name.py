2b03c26086990ea15045f23facd79caba0b494a1
Millana01 <milanovopol68@gmail.com>
2021-05-04 13:29:14 +0300

    Andersen techinal task №2

--- name_task/name.py
+++ name_task/name.py
@@ -7,7 +7,7 @@ while True:
     print('Пока')
     break
   elif input_name == target_name:
-    print('Привет, %s' % target_name.capitalize())
+    print('Привет, %s!' % target_name.capitalize())
   else:
     print('Нет такого имени')
     
